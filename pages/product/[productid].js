af633a376f15b6bfa20b335911d1d4a88dc754eb
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-12-04 20:53:04 +0100

    making api call to fetch the product details

--- pages/product/[productid].js
+++ pages/product/[productid].js
@@ -1,5 +1,8 @@
-function ProductId(){
-    return <h1>Know More About Our Products</h1>
+import {useRouter} from 'next/router'
+function ProductDetails(){
+    const router = useRouter()
+    const ProductId = router.query.ProductId
+    return <h1>Know More About Our Products{ProductId}</h1>
 }
 
-export default ProductId
+export default ProductDetails
