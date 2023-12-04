b04e48e61f5942824d3fd1840f421c2c9768c453
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-12-04 20:19:39 +0100

    productlist

---
+++ pages/product/index.js
@@ -0,0 +1,11 @@
+function ProductList(){
+    return(
+        <>
+        <h2>water proof</h2>
+        <h2>sand proof</h2>
+        <h2>salt</h2>
+        </>
+    )
+}
+
+export default ProductList
