04a8d94c7c3b2214e16bd5c52368008ae211318a
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-12-05 14:53:52 +0100

    sandal landing pages

--- pages/product/[productid].js
+++
@@ -1,8 +0,0 @@
-import {useRouter} from 'next/router'
-function ProductDetails(){
-    const router = useRouter()
-    const ProductId = router.query.ProductId
-    return <h1>Know More About Our Products{ProductId}</h1>
-}
-
-export default ProductDetails
---
+++ pages/product/sandal.js
@@ -0,0 +1,5 @@
+function Sandal(){
+    return <h1>Sandal Landing page</h1>
+}
+
+export default Sandal
