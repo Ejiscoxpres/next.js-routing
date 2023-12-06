d17d0fae22bffc3a34b5831b39a3988c34bd7b93
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-12-06 12:52:15 +0100

    link component navigation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from "next/link"
+
 function Home(){
-    return <h1>Home page</h1>
+    return (
+     <div>
+        <h1>Home page</h1>
+        <link href='/blog'>
+        <a>Blog</a>
+        </link>
+    </div>
+    )
 
 }
 export default Home
