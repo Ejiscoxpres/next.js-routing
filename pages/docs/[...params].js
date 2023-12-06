23342488d9558ab2a3cfbe2d56907d3ebff01e68
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-12-06 09:27:17 +0100

    params feature

--- pages/docs/[...params].js
+++ pages/docs/[...params].js
@@ -3,7 +3,18 @@ function Doc(){
     const router= useRouter()
     const {params =[]} = router.query
     console.log(params)
+    if (params.length ===2){
+        return( <h1>viewingdocs for feature {params[0]} and concept {params[1]}
+        </h1>
+        )
+
+    } else if (params.length===1){
+        return (
+            <h1>viewing  docs for feature{params[0]}</h1>
+        )
+    }
     return <h1>document home page</h1>
+
 }
 
 export default Doc
